62d5a21d1918ceb2a581a6fc626e8842c883be2d
Camden Narzt <c.narzt@me.com>
2025-02-19 06:37:54 -0700

    include newer functions

--- src/getargv.c
+++ src/getargv.c
@@ -10,14 +10,14 @@ bool get_argv_of_pid(const struct GetArgvOptions *options,
 }
 
 void free_ArgvArgcResult(struct ArgvArgcResult *result) {}
-
+void free_ArgvEnvpResult(struct ArgvEnvpResult* result) {}
 void free_ArgvResult(struct ArgvResult *result) {}
 
 bool get_argv_and_argc_of_pid(pid_t pid, struct ArgvArgcResult *result) {
   return true;
 }
 
-int32_t get_arg_exact(pid_t pid) { return 0; }
+int32_t get_arg_exact_size(pid_t pid) { return 0; }
 
 bool print_argv_of_pid_to(const char *start_pointer, const char *end_pointer,
                           FILE *outstream) {
@@ -26,5 +26,9 @@ bool print_argv_of_pid_to(const char *start_pointer, const char *end_pointer,
 
 bool get_argv_of_pid_no_malloc(const struct GetArgvOptions *options,
                                struct ArgvResult *retVal, size_t argsize) {
+  return true;
+}
+
+bool get_argv_and_envp_of_pid(pid_t pid, struct ArgvEnvpResult* result){
   return true;
 }
